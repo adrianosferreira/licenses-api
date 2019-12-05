5b9f68c089dcda05dfb5644025a2b00ecefd010e
Adriano Ferreira <adriano.f@onthegosystems.com>
2019-12-05 17:29:57 -0200

    fixup

--- app/src/Security/TokenAuthenticator.php
+++ app/src/Security/TokenAuthenticator.php
@@ -20,19 +20,10 @@ class TokenAuthenticator extends AbstractGuardAuthenticator {
 		$this->em = $em;
 	}
 
-	/**
-	 * Called on every request to decide if this authenticator should be
-	 * used for the request. Returning false will cause this authenticator
-	 * to be skipped.
-	 */
-	public function supports( Request $request ) {
+	public function supports( Request $request ): bool {
 		return $request->headers->has( 'X-AUTH-TOKEN' );
 	}
 
-	/**
-	 * Called on every request. Return whatever credentials you want to
-	 * be passed to getUser() as $credentials.
-	 */
 	public function getCredentials( Request $request ) {
 		return [
 			'token' => $request->headers->get( 'X-AUTH-TOKEN' ),
@@ -46,37 +37,25 @@ class TokenAuthenticator extends AbstractGuardAuthenticator {
 			return;
 		}
 
-		// if a User object, checkCredentials() is called
 		return $this->em->getRepository( User::class )->findOneBy( [ 'token' => $apiToken ] );
 	}
 
 	public function checkCredentials( $credentials, UserInterface $user ) {
-		// check credentials - e.g. make sure the password is valid
-		// no credential check is needed in this case
-
-		// return true to cause authentication success
 		return true;
 	}
 
 	public function onAuthenticationSuccess( Request $request, TokenInterface $token, $providerKey ) {
-		// on success, let the request continue
 		return null;
 	}
 
 	public function onAuthenticationFailure( Request $request, AuthenticationException $exception ) {
 		$data = [
 			'message' => strtr( $exception->getMessageKey(), $exception->getMessageData() )
-
-			// or to translate this message
-			// $this->translator->trans($exception->getMessageKey(), $exception->getMessageData())
 		];
 
 		return new JsonResponse( $data, Response::HTTP_FORBIDDEN );
 	}
 
-	/**
-	 * Called when authentication is needed, but it's not sent
-	 */
 	public function start( Request $request, AuthenticationException $authException = null ) {
 		$data = [
 			'error' => 'Authentication Required'
